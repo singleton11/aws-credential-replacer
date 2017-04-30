34cd2a35edd37fab4442565c64ee66cdb459e013
Anton Prokhorov <prokhorov@saritasa.com>
2017-04-30 18:56:52 +0700

    Prototype initial version of utility

--- replacer.py
+++ replacer.py
@@ -1,8 +1,42 @@
 #!/usr/bin/env python
+import os
+import sys
+
+import click
+from credstash import getSecret, listSecrets
+from jinja2 import Environment, FileSystemLoader
 
 
-def main():
-    pass
+def render_with_credentials(file):
+    """Render file argument with credstash credentials
+
+    Load file as jinja2 template and render it with context where keys are 
+    credstash keys and values are credstash values
+
+    Args:
+        file (str): jinja2 template file path
+
+    Returns:
+        str: Rendered string
+    
+    """
+    env = Environment(loader=FileSystemLoader(os.path.dirname(file)))
+    template = env.get_template(os.path.basename(file))
+    context = {secret['name']: getSecret(secret['name'])
+               for secret in listSecrets()}
+    return template.render(**context)
+
+
+@click.command()
+@click.argument('file')
+def main(file):
+    """Output rendered template
+
+    Args:
+        file (str): jinja2 template file path
+
+    """
+    sys.stdout.write(render_with_credentials(file))
 
 
 if __name__ == '__main__':
