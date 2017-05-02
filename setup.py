0eb195b7837792a3be56bc4beb1984b1ffd20ae9
Anton Prokhorov <prokhorov@saritasa.com>
2017-05-02 19:52:22 +0700

    Bump to 0.1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ required = [
 
 setup(
     name='aws_credentials_replacer',
-    version='0.1.5',
+    version='0.1.6',
     description='AWS credentials replacer',
     author='Anton Prokhorov',
     author_email='betrayer11@gmail.com',
